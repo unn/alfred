195985924c6a3a25f3a3d5ea9d682e116c145bee
David Stoline <drupal@davidstoline.com>
2011-10-16 16:36:05 -0400

    Refactoring: Removed script from plist to make it a little simplier to contribute and getting rid of the wonkey arg handling

---
+++ Drupal/drupal-alfred.sh
@@ -0,0 +1,53 @@
+DRUSH_PATH="/usr/local/bin/drush"
+
+#arg handling in alfred sucks, lets split it up
+#query="{query}"
+#IFS=" "
+#set -- $query
+
+proj=''
+site=''
+
+gotodo() {
+  open http://${site}drupal.org/${proj}/${nid}
+}
+
+check() {
+  if [[ "$nid" =~ ^[0-9]+$ ]] ; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+case "$1" in
+  d)
+    shift
+    ${DRUSH_PATH} $@
+    ;;
+  g)
+    nid=$2
+    check
+    proj="node"
+    site="groups."
+    gotodo
+    ;;
+  m|t|p)
+    proj="project"
+    site=""
+    nid=$2
+    gotodo
+    ;;
+  *)
+    nid=$1
+    site=""
+    if check; then
+      proj="node"
+    else
+      proj="project"
+    fi
+    gotodo
+    ;;
+esac
+
+exit 0
